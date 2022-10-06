1610d8df8d9c90048307b8cbe78781150a43b5a0
AlexSells <115185894+AlexSells@users.noreply.github.com>
2022-10-06 23:01:00 +0200

    Update Juego_SQL.sql

--- Juego_SQL.sql
+++ Juego_SQL.sql
@@ -13,9 +13,9 @@ CREATE TABLE Jugadors (
  
 CREATE TABLE Partida (
   Id INTEGER AUTO_INCREMENT,
-  Inicio DATETIME DEFAULT CURRENT_TIMESTAMP,
+  Inici DATETIME DEFAULT CURRENT_TIMESTAMP,
   Final DATETIME DEFAULT CURRENT_TIMESTAMP,
-  Ganador VARCHAR(30) NOT NULL,
+  Ganador VARCHAR(30),
   PRIMARY KEY (id)
 )ENGINE = InnoDB;
 
@@ -23,29 +23,30 @@ CREATE TABLE Participacio (
   Id_Usuari INTEGER,
   Id_Partida INTEGER,
   Posicio INTEGER,
-  FOREIGN KEY (user_id) REFERENCES Jugador(Id),
-  FOREIGN KEY (game_id) REFERENCES Partida(Id) 
+  FOREIGN KEY (Id_Usuari) REFERENCES Jugadors(Id),
+  FOREIGN KEY (Id_Partida) REFERENCES Partida(Id) 
 )ENGINE = InnoDB;
 
-INSERT INTO Jugadors(username, password, email) VALUES ("Ana", sha2("1111", 256), "Ana@gmail.com,1");
-INSERT INTO Jugadors(username, password, email) VALUES ("Taha", sha2("2222", 256), "Taha@gmail.com,1");
-INSERT INTO Jugadors(username, password, email) VALUES ("Alex", sha2("3333", 256), "Alex@gmail.com,1");
-INSERT INTO Jugadors(username, password, email) VALUES ("Juan", sha2("3333", 256), "Juan@gmail.com,1");
+INSERT INTO Jugadors(Usuari, Contrasenya, Email,Conectat) VALUES ("Ana", sha2("1111", 256), "Ana@gmail.com",1);
+INSERT INTO Jugadors(Usuari, Contrasenya, Email,Conectat) VALUES ("Taha", sha2("2222", 256), "Taha@gmail.com",1);
+INSERT INTO Jugadors(Usuari, Contrasenya, Email,Conectat) VALUES ("Alex", sha2("3333", 256), "Alex@gmail.com",1);
+INSERT INTO Jugadors(Usuari, Contrasenya, Email,Conectat) VALUES ("Juan", sha2("3333", 256), "Juan@gmail.com",0);
 
 
-INSERT INTO Partida(end_date) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
-INSERT INTO Partida(end_date) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
-INSERT INTO Partida(end_date) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
-INSERT INTO Partida(end_date) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
-INSERT INTO Partida(end_date) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
-INSERT INTO Partida(end_date) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
+INSERT INTO Partida(Final) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
+INSERT INTO Partida(Final) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
+INSERT INTO Partida(Final) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
+INSERT INTO Partida(Final) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
+INSERT INTO Partida(Final) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
+INSERT INTO Partida(Final) VALUES (DATE_ADD(CURRENT_TIMESTAMP, INTERVAL RAND()*15 MINUTE));
 
 
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (1, 1);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (2, 2);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (3, 3);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (4, 4);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (1, 4);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (2, 3);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (3, 2);
-INSERT INTO Participacio(Id_Usuari, Id_Partida) VALUES (4, 1);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (1, 1, 1);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (2, 1, 2);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (3, 1, 3);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (4, 1, 4);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (1, 2, 4);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (2, 2, 3);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (3, 2, 2);
+INSERT INTO Participacio(Id_Usuari, Id_Partida, Posicio) VALUES (4, 2, 1);
+
